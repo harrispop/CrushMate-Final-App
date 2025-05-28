56cda7f96cac0675722a27bb98daa3e6a3131179
Andrew <harrycheng1115@gmail.com>
2025-05-29 04:10:20 +0800

    better profile page and home layout

--- profilepage/urls.py
+++ profilepage/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('profile/', profile_page, name='profilepage'),
     path('delete/<int:photo_id>/', delete_photo, name='delete_photo'),
     path('set_main/<int:photo_id>/', views.set_main_photo, name='set_main_photo'),
+    path('upload_main_avatar/', views.upload_main_avatar, name='upload_main_avatar'),
+
 ]
