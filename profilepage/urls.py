94100a6b01b7c99f197ca99410969e45fbdfd6a7
yiting00112235 <alina00112235@email.com>
2025-05-28 22:27:35 +0800

    pictureok

--- profilepage/urls.py
+++ profilepage/urls.py
@@ -1,7 +1,9 @@
+from . import views 
 from django.urls import path
 from .views import profile_page, delete_photo
 
 urlpatterns = [
     path('profile/', profile_page, name='profilepage'),
     path('delete/<int:photo_id>/', delete_photo, name='delete_photo'),
+    path('set_main/<int:photo_id>/', views.set_main_photo, name='set_main_photo'),
 ]
